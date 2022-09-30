f67271d970d419403e5510c40227704b3276513e
Sean G <seangan.en@Gmail.com>
2022-09-30 20:19:07 +1000

    keyboard basic functions are operational

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ function removeTransition(e) {
 function keyPress(e){
   const thock = document.getElementById('thock')
   const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  const line = document.querySelector('[data-input]');
+  const prev = document.querySelector('[data-ouput]');
+  const prev2 = document.querySelector('[data-output-2]')
+  const prev3 = document.querySelector('[data-output-3]')
   if(!thock) return;
   thock.currentTime = 0;
   thock.play();
@@ -25,6 +29,47 @@ function keyPress(e){
   key.addEventListener('keyup', () =>{
     key.classList.remove('keyboard__key--down')
   })
+  switch (e.keyCode){
+    case 27:
+    case 9:
+    case 220:
+    case 20:
+    case 16:
+    case 38:
+    case 46:
+    case 17:
+    case 18:
+    case 91:
+    case 93:
+    case 18:
+    case 37:
+    case 40:
+    case 39:
+      return;
+    case 13:
+      prev3.innerText = prev2.innerText;
+      prev2.innerText = prev.innerText;
+      prev.innerText = line.innerText;
+      line.innerText = '';
+      break;
+    case 32:
+      line.innerText += '\u00a0';
+      break;
+    case 8:
+      const textSeg = line.innerText;
+      if (textSeg.length == 0) {return}
+      line.innerText = textSeg.slice(0,textSeg.length-1);
+      break;
+    default:
+      line.innerText += key.innerText;
+      break;
+  }
+ 
+  
+}
+
+function backspace(e){
+
 }
 
 
