0d78ec80e2fb4bf58b0a2e294c66082ba2e95d70
Sean G <seangan.en@Gmail.com>
2022-10-04 08:04:36 +1100

    Intergated 4 line memory feature

--- script.js
+++ script.js
@@ -12,16 +12,35 @@ For this project we want to
 function removeTransition(e) {
   const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
   key.classList.remove('keyboard__key--down')
-  
 }
+
+function capsLockDown(e){
+  const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  if (e.keyCode == 20) {
+    console.log(e.ModifierState('CapsLock'))
+  }
+}
+
+function capsLockUp(e){
+  const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  if (e.keyCode == 20) {
+    
+  }
+}
+
+function capsLockUp(e){
+  const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+}
+
 
 function keyPress(e){
   const thock = document.getElementById('thock')
   const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
   const line = document.querySelector('[data-input]');
-  const prev = document.querySelector('[data-ouput]');
+  const prev = document.querySelector('[data-output]');
   const prev2 = document.querySelector('[data-output-2]')
   const prev3 = document.querySelector('[data-output-3]')
+  const speed = document.querySelector('[data-speed]');
   if(!thock) return;
   thock.currentTime = 0;
   thock.play();
@@ -33,7 +52,6 @@ function keyPress(e){
     case 27:
     case 9:
     case 220:
-    case 20:
     case 16:
     case 38:
     case 46:
@@ -45,33 +63,41 @@ function keyPress(e){
     case 37:
     case 40:
     case 39:
+    case 20:
       return;
-    case 13:
+    case 13: //Enter
       prev3.innerText = prev2.innerText;
       prev2.innerText = prev.innerText;
       prev.innerText = line.innerText;
       line.innerText = '';
       break;
-    case 32:
+    case 32: //Space
       line.innerText += '\u00a0';
       break;
-    case 8:
+    case 8: //Backspace
       const textSeg = line.innerText;
-      if (textSeg.length == 0) {return}
+      if (textSeg.length == 0) {
+        line.innerText = prev.innerText;
+        prev.innerText = prev2.innerText;
+        prev2.innerText = prev3.innerText;
+        prev3.innerText = '';
+        return
+      }
       line.innerText = textSeg.slice(0,textSeg.length-1);
+
       break;
     default:
-      line.innerText += key.innerText;
+      if (caps == 0){
+        line.innerText += (key.innerText).toLowerCase();
+      } else {
+        line.innerText += key.innerText;
+      }
       break;
   }
- 
-  
-}
-
-function backspace(e){
 
 }
 
-
+let caps = 0;
 window.addEventListener('keydown', keyPress);
 window.addEventListener('keyup',removeTransition)
+window.addEventListener('keydown', capsLockDown)
